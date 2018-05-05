3c11d9f339b2da6f418fc34f8561613f11d5dd1f
Quan Dong <604925267@qq.com>
2018-05-05 21:37:06 +0800

    添加些备注

--- CSDN/NoName.py
+++ CSDN/NoName.py
@@ -1,14 +1,16 @@
 # 爬取CSDN数据文件
 
-# 还没有解决= {
-#     'ajax': '',
-#     '去重': [
-#         '暴力插入到HASH中',
-#         '在爬取阶段只访问之前没有访问过的：但是如果ip在变 如何知道哪些之前爬过？',
-#     ],
-#     '代理-防止被封ip':'',
-#     'headers':'不完善',
-# }
+# 环境
+# Anaconda3      python 环境
+# redis          python操作redis的库
+# requests       访问网页用的
+# BeautifulSoup  HTML解析库
+
+# 插入redis时采用HASH
+# key:field:value => CSDN:hash(title+':'+time):[title+':'+time]
+
+# 直接在html中解析出title
+# 由于server返回的json也没有时间,所以对每个title访问其文章所在网页获取时间
 
 # -*- coding: utf-8 -*-
 
@@ -18,22 +20,32 @@ import redis
 import requests
 from bs4 import BeautifulSoup
 
-# 解决乱码
-import io
-import sys
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')  # 改变标准输出的默认编码
-# 解决乱码结束
+SCHEDULE = True
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True, db=2)
 
-def insert(info,time):
+def insert(key,info,time):
     id = hash(info+':'+time)
-    print(id,info+':'+time)
-    r.hset('CSDN',id,info+':'+time)
+    if r.hexists(key, id):
+        pass
+    else:
+        print(id,':\n',info+':'+time)
+        r.hset(key, id, info+':'+time)
+        if(SCHEDULE): print('---------------->>>>数据已插入redis数据库\n')
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Cookie': 'uuid_tt_dd=10_19746667890-1517545658385-746207; Hm_ct_6bcd52f51e9b3dce32bec4a3997715ac=1788*1*PC_VC; kd_user_id=232eab61-645a-4aa5-a31b-eb326ea4199a; UN=qq_33184171; __yadk_uid=OonBfwi6PAqK1J5FXPDtCMNEZORRQdE7; __message_sys_msg_id=0; __message_gu_msg_id=0; __message_cnel_msg_id=0; __message_in_school=0; dc_session_id=10_1525407336381.331877; smidV2=20180504121546487688887a90d7cb34e4f8bda3060fd4003ebe7b21de7fc00; kd_0e1a1f29-37da-4c44-8a33-b4735dc85f10_kuickDeal_pageIndex=0; kd_0e1a1f29-37da-4c44-8a33-b4735dc85f10_kuickDeal_leaveTime=1525407376813; BT=1525407375396; ADHOC_MEMBERSHIP_CLIENT_ID1.0=49a3019b-b278-a70a-9bef-bbd1832e4634; TY_SESSION_ID=3f5f4a07-df4b-4136-b4bd-c0cd2c99ff1d; Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac=1525497563,1525498773,1525499121,1525505588; dc_tos=p88vy0; Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac=1525505993',
+    'Host': 'blog.csdn.net',
+    'Referer': 'https://blog.csdn.net/nav/cloud',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-Tingyun-Id': 'wl4EtIR_7Is;r=505993350',
 }
+
 
 def get_time(_url):
     wb_data = requests.get(_url, headers=headers)
@@ -44,18 +56,26 @@ def get_time(_url):
         # print(x)
         return x.text
 
+
 def get_info(url):
+    if (SCHEDULE): print('----------------------')
+    if (SCHEDULE): print('开始访问网站')
     wb_data = requests.get(url, headers=headers)
     # wb_data.encoding = 'utf-8'
     soup = BeautifulSoup(wb_data.text, "lxml")
-    titles = soup.select('a[strategy="new"]')
 
+    if(SCHEDULE): print('访问成功,准备爬取数据')
+
+    titles = soup.select('a[strategy="new"]')
+    titles.extend(soup.select('a[strategy="hot"]'))
     for title in titles:
         _url = title.get('href')  # 获取这个博文的链接 访问得到time
         time = get_time(_url)     
         info = title.text.strip() 
-        insert(info,time)         # 插入到redis数据库中
-    
+        insert('CSDN', info, time)         # 插入到redis数据库中
+
+    if(SCHEDULE): print('爬取数据成功')
+
     # 测试用的
     # print(type(soup))
     # print(" ---------------------------- ")
@@ -69,6 +89,8 @@ def get_info(url):
 
 if __name__ == '__main__':
     url = 'https://blog.csdn.net/nav/cloud'
-    # while(true):
-    get_info(url)
-    # time.sleep(60)  # 休眠1s
+    while(True):
+        get_info(url)
+        time.sleep(10)  # 休眠10s
+    
+    print('PROGRAM END')
